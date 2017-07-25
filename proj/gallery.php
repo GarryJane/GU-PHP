a7b07cdf3d2e7c2a2da12aaec3ec438fa24f480c
GarryJane <mmm53.ripe@gmail.com>
2017-07-26 00:39:51 +0300

    Закончена галерея изображений миниатюр, hw2

--- proj/gallery.php
+++ proj/gallery.php
@@ -2,21 +2,29 @@
 
 require_once 'vendor/autoload.php';
 
+// массив изображений галереи
 $imgArr = [
-    'img/gallery1.jpg',
-    'img/gallery2.jpg',
-    'img/gallery3.jpg',
-    'img/gallery4.jpg'
+    [
+        'name' => "img1",
+        'url' => 'img/gallery-1.jpg'
+    ],
+    [
+        'name' => "img2",
+        'url' => 'img/gallery-2.jpg'
+    ],
+    [
+        'name' => "img3",
+        'url' => 'img/gallery-3.jpg'
+    ],
+    [
+        'name' => "img4",
+        'url' => 'img/gallery-4.jpg'
+    ]
 ];
 
 $loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader, array(
-    'cache' => 'tmp/compilation_cache',
-));
+$twig = new Twig_Environment($loader, array('cache' => 'tmp/compilation_cache'));
 
 $template = $twig->load('gallery.tmpl');
-echo $template->render([
-        "imgArr" => $imgArr,
-        "test" => "test"
-]);
+echo     $template->render( array("imgs" => $imgArr) );
 
