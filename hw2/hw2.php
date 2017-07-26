4f2d1ecc2b69c164131ea8f061322d90ec24b62a
Garry Jane <mmm53.ripe@gmail.com>
2017-07-26 12:41:44 +0300

    ide settings

--- hw2/hw2.php
+++ hw2/hw2.php
@@ -1,7 +1,36 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: ddl
- * Date: 17.07.17
- * Time: 13:45
- */
+
+class A
+{
+    const M = 1, N = 2;
+
+    public function __set($name, $value)
+    {
+        echo "нет такого метода $name, $value не записано\n";
+    }
+
+    public function __get($name)
+    {
+        echo "не читаицО ! нет такого метода $name\n";
+    }
+
+    public function __call($name, $arguments)
+    {
+        echo "вызван несуществующий метод $name с аргументами:\n";
+        var_dump($arguments);
+    }
+
+    public function __toString()
+    {
+        return "не получится использовать это как переменную, это - объект )\n";
+    }
+
+}
+
+$a = new A();
+
+echo $a->b;
+$a->c = 2;
+echo $a::M . "\n" . $a::N . "\n";
+$a->test(1,2,3);
+echo $a;
